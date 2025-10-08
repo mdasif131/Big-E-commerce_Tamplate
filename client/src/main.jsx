4e8b04fdb9ce94321c9d97b50a125994aef41936
mdasif131 <asifmdasifchy@gmail.com>
2025-10-08 18:22:14 +0600

    redux setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App.jsx';
 import { Route, RouterProvider, createRoutesFromElements } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />} /> 
-  )
+  createRoutesFromElements(<Route path="/" element={<App />} />)
 );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
