9693e35ca1753e03948a7eedbf3c2e2a6c32f5a8
mdasif131 <asifmdasifchy@gmail.com>
2025-11-27 11:50:29 +0600

    category backend api setup

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 // packages import
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import path from "path"; 
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 
 
 // Utils import
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000; 
@@ -23,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/api/auth", userRoutes);
+app.use('/api/category' , categoryRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
