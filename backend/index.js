3aa2c1625888ba25d2ecde431a65f5ac47b7be1e
mdasif131 <asifmdasifchy@gmail.com>
2025-11-28 22:15:30 +0600

    Uploads file api setup by multer

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 
 // Utils import
@@ -10,7 +11,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-
+import uploadRoutes from "./routes/uploadRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000; 
 
@@ -26,6 +27,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", userRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/uploads', uploadRoutes);
+
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
+
+
+app.get('/',(req, res) => {
+  res.send("API is running...");
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
